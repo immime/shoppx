2b30e4f06c81e33b5e21aecc70d2eb4c5198a139
samgrigg <samgrigg@gmail.com>
2014-06-26 16:56:26 -0600

    added category navigation directive. Added basic menu & header styles

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,9 +3,21 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []).
-  directive('appVersion', ['version', function(version) {
+angular.module('myApp.directives', [])
+  .directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }]);
+  }])
+  .directive('sngCategoryNavigation', function() {
+    console.log('categories directive');
+    return {
+      restrict: "A",//Declare on a <ul> or <ol> element
+      template: '<li ng-repeat="category in categories">\
+        <a href="#/category/{{category.id}}/">{{category.title}}</a>\
+      </li>',
+      link: function(scope, elem, attrs) {
+        elem.addClass('menu-list');
+      }
+    }
+  });
